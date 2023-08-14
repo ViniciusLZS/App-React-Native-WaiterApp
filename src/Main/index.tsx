7fb4aab3f91e043e640e7a4480a9ed2bcc1b562b
Vinicius <vinicius.luiz.9256@gmail.com>
2023-08-14 17:24:25 -0300

    first commit

---
+++ src/Main/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from '../components/Text';
+
+import * as S from './styles';
+
+export function Main() {
+  return (
+    <S.Container>
+      <Text>Render Main</Text>
+    </S.Container>
+  );
+}
