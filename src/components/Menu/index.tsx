e798c51f85bdc951a7e4841e8f1d20748023205d
Vinicius <vinicius.luiz.9256@gmail.com>
2023-08-16 14:21:54 -0300

    feat: menu created

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,3 +1,40 @@
+import { FlatList } from 'react-native';
+import { products } from '../../mocks/products';
+import { Text } from '../Text';
+import * as S from './styles';
+import { formatCurrent } from '../../utils/formatCurrency';
+import { PlusCircle } from '../Icons/PlusCircle';
+
 export function Menu() {
-  return null;
+  return (
+    <FlatList
+      data={ products }
+      style={ {marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      keyExtractor={product => product._id}
+      ItemSeparatorComponent={S.Separator}
+      renderItem={({ item: product }) => (
+        <S.Product>
+          <S.Image
+            source={{
+              uri: `http://192.168.18.7:3001/uploads/${product.imagePath}`
+            }}
+          />
+          <S.ProductDetails>
+            <Text weight="600">{product.name}</Text>
+
+            <Text size={14} color="#666" style={{ marginVertical: 8 }}>
+              {product.description}
+            </Text>
+
+            <Text size={14} weight="600">{formatCurrent(product.price)}</Text>
+          </S.ProductDetails>
+
+          <S.AddToCartButton>
+            <PlusCircle />
+          </S.AddToCartButton>
+        </S.Product>
+      )}
+    />
+  );
 }
